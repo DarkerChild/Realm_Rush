1d2f98380f8f74d799d12d5e855df30c5785401f
Darker <wkc419@hotmail.com>
2020-04-18 16:52:44 +0100

    End of chapter 127. Cube now snaps to position and had label with X,Z co-ordinates in grid.

---
+++ Assets/CubeLabel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeLabel : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        TextMesh text = GetComponent<TextMesh>();
+        text.text = transform.position.x + "," + transform.position.z;
+    }
+}
--- Assets/EditorSnap.cs
+++ Assets/EditorSnap.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class EditorSnap : MonoBehaviour
 {
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
     void Update()
+    {
+        SnapPosition();
+        UpdateLabel();
+    }
+
+    private void UpdateLabel()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = transform.position.x/gridSize + "," + transform.position.z/gridSize;
+    }
+
+    private void SnapPosition()
     {
         Vector3 snapPosition;
         snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
