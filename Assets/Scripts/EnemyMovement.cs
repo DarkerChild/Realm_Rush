9b0827f165b5ff05d016782c64cda934ed1516f0
Darker <wkc419@hotmail.com>
2020-04-19 13:32:54 +0100

    Moved folders around.  At end of chapter 130 now.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = null;
+    [SerializeField] float waitTime = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath(waitTime));
+    }
+
+    IEnumerator FollowPath(float waitTime)
+    {
+        print("Starting Patrol");
+        foreach (Waypoint waypoint in path)
+        {
+            print("Moving to position: " + waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+        print("Finished Partol");
+    }
+}
